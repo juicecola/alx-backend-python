2e754fadee4a2e38f0af4de6f6012036c96c7f2a
Eric Munyi <irerieric@gmail.com>
2023-07-11 08:47:07 +0000

    #!/usr/bin/env python3

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3i
+"""coroutine called async_generator that takes no arguments"""
+import asyncio
+import random
+
+async def async_generator():
+    """Async Generator"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
